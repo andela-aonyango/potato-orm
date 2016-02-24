756c37524af681e920876748e1cd9a3eeba34c3e
andela-aonyango <andela-aonyango@users.noreply.github.com>
2016-02-24 15:36:03 +0300

    Added a test class for the Person class

---
+++ tests/PersonTest.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @file     PersonTest.php
+ * This class tests the functions in the Database class
+ * @package  PotatoORM
+ * @author   andrew <andrew.onyango@andela.com>
+ * @license  MIT => https://opensource.org/licenses/MIT
+ */
+
+namespace PotatoORM\Test;
+
+use PotatoORM\Models\Person;
+use PHPUnit_Framework_TestCase;
+
+/**
+ *Tests for the Entity class and classes that derive from it
+ *
+ * @category Test
+ * @package  PotatoORM
+ */
+class PersonTest extends PHPUnit_Framework_TestCase
+{
+    public function testPersonProperties()
+    {
+        $person = new Person();
+        $person->first_name = "john";
+        $person->last_name = "doe";
+        $person->age = 23;
+        $person->gender = "female";
+
+        $this->assertEquals("john", $person->first_name);
+        $this->assertEquals("doe", $person->last_name);
+        $this->assertEquals(23, $person->age);
+        $this->assertEquals("female", $person->gender);
+    }
+}
